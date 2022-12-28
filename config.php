e38fb23a13355f2ec5e2dc71212f496e931b15b9
Sabina Arnt <64563882+sabinaarnt@users.noreply.github.com>
2022-12-28 11:55:23 -0300

    Update config.php

--- config.php
+++ config.php
@@ -6,16 +6,7 @@ $nome_sistema = "Daupause Admin";
 $email_adm = 'admin@hotmail.com';
 $url_sistema = "http://localhost/pdv/";  // eh preciso configurar essa url para gerar os relatorios, ela deve apontar para a raiz do dominio com a barra no final e o protocolo http ou https de acordo com seu dominio no inicio.
 
-$telefone_sistema = "(85) 98751-1685";
-$endereco_sistema = "Shopping Del Paseo";
-$rodape_relatorios = "Sistema desenvolvido por Sabina Silva";
 
-//VARIAVEIS PARA O BANCO DE DADOS LOCAL
-
-$servidor = 'localhost';
-$usuario = 'root';
-$senha = '';
-$banco = 'pdv';
 
 //VARIAVEIS DE CONFIGURACOES DO SISTEMAS
 $relatorio_pdf = 'Sim'; 
